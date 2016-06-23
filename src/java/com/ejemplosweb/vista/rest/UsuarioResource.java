83dc9fbd28bb0d652d5bf1efd9e6dbe450b8d738
Angel Torres <jatorresdev@gmail.com>
2016-06-23 00:22:55 -0500

    Se añaden metodos de post, put y delete a restful de Usuario

--- src/java/com/ejemplosweb/vista/rest/UsuarioResource.java
+++ src/java/com/ejemplosweb/vista/rest/UsuarioResource.java
@@ -3,14 +3,20 @@ package com.ejemplosweb.vista.rest;
 import com.ejemplosweb.controlador.UsuarioControlador;
 import com.ejemplosweb.controlador.excepciones.EjemploMovilesException;
 import com.ejemplosweb.modelo.vo.UsuarioVO;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.List;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.Produces;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PUT;
+import javax.ws.rs.POST;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.core.MediaType;
 
 /**
@@ -35,7 +41,7 @@ public class UsuarioResource {
      * @return an instance of com.ejemplosweb.modelo.vo.UsuarioVO
      */
     @GET
-    //@Path("consultar") 
+    //@Path("consultar")
     @Produces(MediaType.APPLICATION_JSON)
     public List<UsuarioVO> getListaUsuarios() throws EjemploMovilesException {
         try {
@@ -48,13 +54,69 @@ public class UsuarioResource {
         }
     }
     
-    
-    /**
-     * PUT method for updating or creating an instance of UsuarioResource
-     * @param content representation for the resource
-     */
     @PUT
-    @Consumes(MediaType.APPLICATION_JSON)
-    public void putJson(UsuarioVO content) {
+    @Path("/modificar")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    public UsuarioVO actualizarUsuario(@FormParam("id") int id,
+            @FormParam("nombre") String nombre,
+            @FormParam("apellido") String apellido,
+            @FormParam("fechaNacimiento") String fechaNacimiento,
+            @FormParam("correo") String correo,
+            @FormParam("clave") String clave) throws EjemploMovilesException, ParseException {
+        
+        SimpleDateFormat formato = new SimpleDateFormat("dd/MM/yyyy");
+        
+        UsuarioVO voUsu = new UsuarioVO();
+        voUsu.setIdUsuario(id);
+        voUsu.setNombreUsuario(nombre);
+        voUsu.setApellidoUsuario(apellido);
+        voUsu.setFechaNacimientoUsuario(formato.parse(fechaNacimiento));
+        voUsu.setCorreoUsuario(correo);
+        voUsu.setClaveUsuario(clave);
+        
+        UsuarioControlador controlUsu = new UsuarioControlador();
+        controlUsu.modificar(voUsu);
+        controlUsu.terminarTransaccion();
+        
+        return voUsu;
+    }
+    
+    @POST
+    @Path("/insertar")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    public UsuarioVO insertarUsuario(@FormParam("nombre") String nombre,
+            @FormParam("apellido") String apellido,
+            @FormParam("fechaNacimiento") String fechaNacimiento,
+            @FormParam("correo") String correo,
+            @FormParam("clave") String clave) throws EjemploMovilesException, ParseException {
+        
+        SimpleDateFormat formato = new SimpleDateFormat("dd/MM/yyyy");
+        
+        UsuarioVO voUsu = new UsuarioVO();
+        voUsu.setNombreUsuario(nombre);
+        voUsu.setApellidoUsuario(apellido);
+        voUsu.setFechaNacimientoUsuario(formato.parse(fechaNacimiento));
+        voUsu.setCorreoUsuario(correo);
+        voUsu.setClaveUsuario(clave);
+        
+        UsuarioControlador controlUsu = new UsuarioControlador();
+        controlUsu.insertar(voUsu);
+        controlUsu.terminarTransaccion();
+        
+        return voUsu;
+    }
+    
+    @DELETE
+    @Path("/eliminar/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public UsuarioVO deleteUser(@PathParam("id") int id) throws EjemploMovilesException{
+        UsuarioControlador controlUsu = new UsuarioControlador();
+        UsuarioVO voUsu = controlUsu.consultar(id);
+        controlUsu.borrar(voUsu);
+        controlUsu.terminarTransaccion();
+        
+        return voUsu;
     }
 }
